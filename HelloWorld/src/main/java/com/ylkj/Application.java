ea6e73a2842eea3713571d59024778cfbc74f353
王瑞 <82210084+WRhhh@users.noreply.github.com>
2022-02-19 13:36:25 +0800

    Add files via upload

---
+++ HelloWorld/src/main/java/com/ylkj/Application.java
@@ -0,0 +1,15 @@
+package com.ylkj;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+/**
+ * @author : WR
+ * @date:2022/2/9 12:45
+ *  http://localhost:8080/greeting
+ */
+@SpringBootApplication
+public class Application {
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+
+}
